8100099189140a6093c5c69d585ca38454a08cf6
samuelbluefin <samuel@bluefinenterprises.com>
2022-10-12 17:26:46 -0400

    added react and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <p className="text-6xl text-indigo-600 font-extrabold">Telemedicine App</p>
+    </div>
+  );
+}
+
+export default App;

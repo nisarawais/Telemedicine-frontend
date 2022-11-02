0e945fd0f4fa7f4a7725e309fb4ecd87f67798a3
aanisar <55286102+aanisar@users.noreply.github.com>
2022-11-02 11:26:55 -0400

    I have added more fields on registration page

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -4,6 +4,32 @@ const Registration = () => {
   return (
     <div className="bg-gray-100 m-auto max-w-xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8 space-y-6">
       <p className="text-2xl font-extrabold">Registration</p>
+      <div className="row">
+      <div className="column">
+        <label htmlFor="firstName" className="sr-only">
+          First Name
+        </label>
+        <input
+          type="text"
+          name="firstName"
+          id="firstName"
+          className="block w-full rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+          placeholder="First Name"
+        />
+      </div>
+      <div className="column space">
+        <label htmlFor="lastName" className="sr-only">
+          Last Name
+        </label>
+        <input
+          type="text"
+          name="lastName"
+          id="lastName"
+          className="block w-full rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+          placeholder="Last Name"
+        />
+      </div>
+      </div>
       <div>
         <label htmlFor="email" className="sr-only">
           Email
@@ -28,6 +54,19 @@ const Registration = () => {
           autoComplete="password"
           className="block w-full rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
           placeholder="Password"
+        />
+      </div>
+      <div>
+        <label htmlFor="password" className="sr-only">
+          Confirm Password
+        </label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          autoComplete="password"
+          className="block w-full rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+          placeholder="Confirm Password"
         />
       </div>
       <button className="w-full px-10 py-3 text-white font-semibold bg-black rounded-md">
